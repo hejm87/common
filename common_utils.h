698b9abcb2de78782462f7867e9d2d5ef8de620e
hejm87 <84332600he@163.com>
2023-10-20 10:51:13 +0800

    重新整理common_utils函数

--- common_utils.h
+++ common_utils.h
@@ -1,11 +1,89 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
+#include <sys/time.h>
 #include <sys/syscall.h>
 
-inline int gettid()
+#include <thread>
+
+template <class T>
+class Singleton
 {
-    return syscall(SYS_gettid);
-}
+public:
+    static T* instance() {
+        static T obj; 
+        return &obj;
+    }
+};
+
+template <class T>
+class ThreadSingleton
+{
+public:
+    static T* instance() {
+        static thread_local T obj; 
+        return &obj;
+    }
+};
+
+class CommonUtils
+{
+public:
+    inline static long now()
+    {
+        return time(0); 
+    }
+
+    inline static long now_ms()
+    {
+        struct timeval tv; 
+        gettimeofday(&tv, NULL);
+        return tv.tv_sec * 1000 + tv.tv_usec / 1000;
+    }
+
+    template <class... Args>
+    static std::string format_string(const string& fmt, Args... args)
+    {
+        std::string s;
+        auto size = snprintf(NULL, 0, fmt.c_str(), args...) + 1;
+        if (s.capacity() < size) {
+            s.reserve(size);         
+        }
+        snprintf((char*)s.c_str(), s.capacity(), fmt.c_str(), args...);
+        return std::move(s);
+    }
+
+	// 仅日志使用（不具备通用性）
+    std::string date_ms(long time_ms = 0)
+    {
+        char date[128]; 
+        long sec, msec;
+        if (time_ms > 0) {
+            sec  = time_ms / 1000; 
+            msec = time_ms % 1000;
+        } else {
+            struct timeval tv; 
+            gettimeofday(&tv, NULL);
+            sec  = tv.tv_sec;
+            msec = tv.tv_usec / 1000;
+        }
+
+        struct tm tmm;
+        localtime_r(&sec, &tmm);
+        snprintf(
+            date, 
+            sizeof(date), 
+            "%04d-%02d-%02d_%02d:%02d:%02d.%03ld", 
+        	tmTime.tm_year + 1900,
+        	tmTime.tm_mon + 1,
+        	tmTime.tm_mday,
+        	tmTime.tm_hour,
+        	tmTime.tm_min,
+        	tmTime.tm_sec,
+        	msec
+        );
+        return date;
+    }
+};
 
 #endif
